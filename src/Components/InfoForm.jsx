ffc10b096472b008e20b1ff6d7309bebdbb2708f
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2020-10-05 20:19:53 +0530

    OnChange and  OnFocus

--- src/Components/InfoForm.jsx
+++ src/Components/InfoForm.jsx
@@ -5,6 +5,16 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 export const InfoForm = () => {
   const [StartDate, SetStartDate] = useState(new Date());
+  const [FirstName, SetFirstName] = useState("Mohammed");
+  const [LastName, SetLastName] = useState("Zubair");
+  const [PlaceOfBirth, SetPlaceOfBirth] = useState("Nanded");
+  const [Age, SetAge] = useState("30");
+  const [EmailId, SetEmailID] = useState("zubair@gmail.com");
+  const [Weight, SetWeight] = useState("72");
+  const [Height, SetHeight] = useState("5.8");
+  const [Chest, SetChest] = useState("34");
+  const [PermAddress, SetPermAddress] = useState("Chaitanya Nager");
+  const [ContactNo, SetContactNo] = useState("8888777799");
 
   return (
     <div>
@@ -15,13 +25,25 @@ export const InfoForm = () => {
           <Form.Row>
             <Form.Group as={Col} controlId="formFirstname">
               <Form.Label>First Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter First Name" />
+              <Form.Control
+                type="text"
+                placeholder="Enter First Name"
+                value={FirstName}
+                onChange={(e) => SetFirstName(e.target.value)}
+                onFocus={() => SetFirstName("")}
+              />
               <Form.Text className="text-muted"></Form.Text>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formlastname">
               <Form.Label>Last Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter Last Name" />
+              <Form.Control
+                type="text"
+                placeholder="Enter Last Name"
+                value={LastName}
+                onChange={(e) => SetLastName(e.target.value)}
+                onFocus={(e) => SetLastName("")}
+              />
               <Form.Text className="text-muted"></Form.Text>
             </Form.Group>
           </Form.Row>
@@ -39,7 +61,13 @@ export const InfoForm = () => {
 
           <Form.Group controlId="placeofbirth">
             <Form.Label>Place Of Birth</Form.Label>
-            <Form.Control type="text" placeholder="Enter Place Of Birth" />
+            <Form.Control
+              type="text"
+              placeholder="Enter Place Of Birth"
+              value={PlaceOfBirth}
+              onChange={(e) => SetPlaceOfBirth(e.target.value)}
+              onFocus={(e) => SetPlaceOfBirth("")}
+            />
             <Form.Text className="text-muted"></Form.Text>
           </Form.Group>
 
@@ -66,14 +94,26 @@ export const InfoForm = () => {
 
             <Form.Group as={Col} controlId="age">
               <Form.Label>Age</Form.Label>
-              <Form.Control type="text" placeholder="Enter age" />
+              <Form.Control
+                type="text"
+                placeholder="Enter age"
+                value={Age}
+                onChange={(e) => SetAge(e.target.value)}
+                onFocus={() => SetAge("")}
+              />
               <Form.Text className="text-muted"></Form.Text>
             </Form.Group>
           </Form.Row>
 
           <Form.Group controlId="email id">
             <Form.Label>Email ID</Form.Label>
-            <Form.Control type="text" placeholder="Enter email" />
+            <Form.Control
+              type="text"
+              placeholder="Enter email"
+              value={EmailId}
+              onChange={(e) => SetEmailID(e.target.value)}
+              onFocus={() => SetEmailID("")}
+            />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
@@ -82,19 +122,37 @@ export const InfoForm = () => {
           <Form.Row>
             <Form.Group as={Col} controlId="weight">
               <Form.Label>Weight(kgs)</Form.Label>
-              <Form.Control type="text" placeholder="Enter Your Weight" />
+              <Form.Control
+                type="text"
+                placeholder="Enter Your Weight"
+                value={Weight}
+                onChange={(e) => SetWeight(e.target.value)}
+                onFocus={() => SetWeight("")}
+              />
               <Form.Text className="text-muted"></Form.Text>
             </Form.Group>
 
             <Form.Group as={Col} controlId="height">
               <Form.Label>Height</Form.Label>
-              <Form.Control type="text" placeholder="Enter Your Height" />
+              <Form.Control
+                type="text"
+                placeholder="Enter Your Height"
+                value={Height}
+                onChange={(e) => SetHeight(e.target.value)}
+                onFocus={() => SetHeight("")}
+              />
               <Form.Text className="text-muted"></Form.Text>
             </Form.Group>
 
             <Form.Group as={Col} controlId="chest">
               <Form.Label>Chest</Form.Label>
-              <Form.Control type="text" placeholder="Enter chest size" />
+              <Form.Control
+                type="text"
+                placeholder="Enter chest size"
+                value={Chest}
+                onChange={(e) => SetChest(e.target.value)}
+                onFocus={() => SetChest("")}
+              />
               <Form.Text className="text-muted"></Form.Text>
             </Form.Group>
           </Form.Row>
@@ -104,23 +162,38 @@ export const InfoForm = () => {
             <Form.Control
               type="text"
               placeholder="Enter Your Permenent Address"
+              value={PermAddress}
+              onChange={(e) => SetPermAddress(e.target.value)}
+              onFocus={() => SetPermAddress("")}
             />
             <Form.Text className="text-muted"></Form.Text>
           </Form.Group>
 
           <Form.Group controlId="contactno">
             <Form.Label>Contact No</Form.Label>
-            <Form.Control type="text" placeholder="Enter Your Contact No" />
+            <Form.Control
+              type="text"
+              placeholder="Enter Your Contact No"
+              value={ContactNo}
+              onChange={(e) => SetContactNo(e.target.value)}
+              onFocus={() => SetContactNo("")}
+            />
             <Form.Text className="text-muted"></Form.Text>
           </Form.Group>
+          <center>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+            {"   "}
+            <Button Variant="primary" type="cancel">
+              Cancel
+            </Button>
+          </center>
 
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-          {"   "}
-          <Button Variant="primary" type="cancel">
-            Cancel
-          </Button>
+          <p>
+            The First Name Of Applicant is {FirstName} and Last Name Is{" "}
+            {LastName}
+          </p>
         </Form>
       </Container>
     </div>
