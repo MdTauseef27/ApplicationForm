b433170055d5f58a73275887eacb3b0da0c66ee6
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2020-11-14 14:19:52 +0530

    changes

--- src/Components/InfoForm.jsx
+++ src/Components/InfoForm.jsx
@@ -635,7 +635,9 @@ export const InfoForm = () => {
             </Form.Group>
           </Form.Row>
 
-          <Form.Row></Form.Row>
+          <Form.Row>
+            <Form.Group as={Col} controlId="Education"></Form.Group>
+          </Form.Row>
 
           <Form.Row>
             <Form.Group as={Col} controlId="Education"></Form.Group>
