a469a41d27a96f13a18dfb8ec32a336ca993ff08
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2020-11-17 09:21:21 +0530

    created label

--- src/Components/InfoForm.jsx
+++ src/Components/InfoForm.jsx
@@ -650,7 +650,9 @@ export const InfoForm = () => {
               ></Form.Control>
             </Form.Group>
 
-            <Form.Group as={Col} controlId="Salary"></Form.Group>
+            <Form.Group as={Col} controlId="Salary">
+              <Form.Label></Form.Label>
+            </Form.Group>
           </Form.Row>
 
           <Form.Group controlId="address">
