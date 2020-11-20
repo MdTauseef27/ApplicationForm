6853b60b7c8919f2aeedd7d299b4fdcc96f6da4b
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2020-11-20 09:33:21 +0530

    validations

--- src/Components/InfoForm.jsx
+++ src/Components/InfoForm.jsx
@@ -69,7 +69,7 @@ export const InfoForm = () => {
       .min(3, "Too Short")
       .max(20, "Too Long")
       .required("*Name Is Required"),
-    SisterEducation: yupString().min(3, "Too Short"),
+    SisterEducation: yupString().min(3, "Too Short").max(20, "Too Long"),
     email: yupString()
       .email("*Must be a valid email address")
       .max(100, "*Email must be less than 100 characters")
