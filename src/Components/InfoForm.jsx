b4cff48df4f2e244a078ca1d1a3c9ebfa9cbdf91
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2020-11-23 09:45:19 +0530

    validation on sister

--- src/Components/InfoForm.jsx
+++ src/Components/InfoForm.jsx
@@ -644,7 +644,9 @@ export const InfoForm = () => {
                 }
               />
               {formik.touched.SisterName && formik.errors.SisterName && (
-                <Form.Control.Feedback></Form.Control.Feedback>
+                <Form.Control.Feedback>
+                  {formik.errors.SisterName}
+                </Form.Control.Feedback>
               )}
             </Form.Group>
 
