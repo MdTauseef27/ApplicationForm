30b288584abf88d6e3c63f09857782ec1c238ce2
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2020-12-03 09:38:55 +0530

    some changes

--- src/Components/FilterEx.jsx
+++ src/Components/FilterEx.jsx
@@ -80,7 +80,7 @@ export const FilterEx = () => {
       <button value="Kenya" onClick={handleClick}>
         Kenya
       </button>
-      <button value="keyasa" onClick={handleClick}></button>
+      <button onClick={handleClick}></button>
 
       {/*<button value="Columbia" onClick={handleClick}>
         Columbia
