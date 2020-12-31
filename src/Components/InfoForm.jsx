b4c5496ac2c63573597232dc23fb6b084dc7493a
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2020-12-31 10:43:59 +0530

    changes

--- src/Components/InfoForm.jsx
+++ src/Components/InfoForm.jsx
@@ -714,7 +714,7 @@ export const InfoForm = () => {
                 }
               ></Form.Control>
               {formik.touched.SisterSalery && formik.errors.SisterSalery && (
-                <Form.Control.Feedback>
+                <Form.Control.Feedback type="">
                   {formik.errors.SisterSalery}
                 </Form.Control.Feedback>
               )}
